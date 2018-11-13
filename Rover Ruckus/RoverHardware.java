4e046fa382134650873dac5fec02b82f18654e3c
RoboBronco <39787636+RoboBronco@users.noreply.github.com>
2018-11-12 20:40:06 -0700

    Sammie - 11/12/18 - Updated from phone

--- Rover Ruckus/RoverHardware.java
+++ Rover Ruckus/RoverHardware.java
@@ -1,40 +1,15 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
-package org.firstinspires.ftc.robotcontroller.internal;
+package org.firstinspires.ftc.teamcode;
 
 import android.nfc.cardemulation.OffHostApduService;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
 import java.lang.Math;
 
 public class RoverHardware
@@ -45,19 +20,22 @@ public class RoverHardware
     public DcMotor  bl   = null;
     public DcMotor  br  = null;
     public DcMotor  lift    = null;
-    public DcMotor  tilt = null;
+    // public DcMotor  tilt = null;
     public Servo    marker   = null;
-    public Servo    flag   = null;
-    public Servo    lock   = null;
-    public Servo    sample = null;
+    // public Servo    sensorArm = null;
+    // public Servo    flag   = null;
+    // public Servo    lock   = null;
+    // public Servo    sample = null;
+    
+    ColorSensor sensorColor;
 
     /* local OpMode members. */
     HardwareMap hwMap           =  null;
     private ElapsedTime period  = new ElapsedTime();
 
     // Variables for Drive Speed
-    final int FORWARD = 1;
-    final int BACK = -1;
+    final double FORWARD = .5;
+    final double BACK = -0.5;
     final int OFF = 0;
 
     /* Constructor */
@@ -76,7 +54,8 @@ public class RoverHardware
         bl  = hwMap.get(DcMotor.class, "bl");
         br = hwMap.get(DcMotor.class, "br");
         lift = hwMap.get(DcMotor.class, "lift");
-        tilt = hwMap.get(DcMotor.class, "tilt" );
+       
+        // tilt = hwMap.get(DcMotor.class, "tilt" );
         fl.setDirection(DcMotor.Direction.REVERSE);
         bl.setDirection(DcMotor.Direction.REVERSE);
         fr.setDirection(DcMotor.Direction.FORWARD);
@@ -88,7 +67,9 @@ public class RoverHardware
         bl.setPower(0);
         br.setPower(0);
         lift.setPower(0);
-        tilt.setPower(0);
+        
+        
+        // tilt.setPower(0);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
@@ -97,20 +78,25 @@ public class RoverHardware
         bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        tilt.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // tilt.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
         // Define and initialize ALL installed servos.
         marker  = hwMap.get(Servo.class, "marker");
-        flag = hwMap.get(Servo.class, "flag");
-        lock = hwMap.get(Servo.class, "lock");
-        sample = hwMap.get(Servo.class, "sample");
+        // sensorArm = hwMap.get(Servo.class, "sensorArm");
+        // flag = hwMap.get(Servo.class, "flag");
+        // lock = hwMap.get(Servo.class, "lock");
+        // sample = hwMap.get(Servo.class, "sample");
 
 
-        flag.setPosition(0);
+        // flag.setPosition(0);
         marker.setPosition(0);
-        lock.setPosition(0);
-        sample.setPosition(0);
+        // sensorArm.setPosition(0);
+        // lock.setPosition(0);
+        // sample.setPosition(0);
+        
+        // get a reference to the color sensor.
+        sensorColor = hwMap.get(ColorSensor.class, "CD1");
 
     }
 
@@ -287,5 +273,9 @@ public class RoverHardware
         bl.setPower(OFF);
         br.setPower(OFF);
     }
+    
+    
+    
+    
  }
 
