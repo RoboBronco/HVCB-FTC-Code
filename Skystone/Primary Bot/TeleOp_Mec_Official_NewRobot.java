a1144dfa31d3647a5bd1f353fda79f50fad18b8e
CJD14147 <59951080+CJD14147@users.noreply.github.com>
2020-01-25 10:16:24 -0700

    Update TeleOp_Mec_Official_NewRobot.java

--- Skystone/Primary Bot/TeleOp_Mec_Official_NewRobot.java
+++ Skystone/Primary Bot/TeleOp_Mec_Official_NewRobot.java
@@ -1,5 +1,6 @@
 package FTC_2019_2020_Season;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
@@ -17,9 +18,9 @@ public class TeleOp_Mec_Official_NewRobot extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        
-        robot.initNoInitBecauseISFunAndNoMove(hardwareMap);
-        // robot.init(hardwareMap);
+    
+       // robot.initNoInitBecauseISFunAndNoMove(hardwareMap);
+         robot.init(hardwareMap);
         
         
         int superPowerReduction = 1;
@@ -43,14 +44,6 @@ public class TeleOp_Mec_Official_NewRobot extends LinearOpMode {
                 superPowerReduction = 5;
             }else{
                 superPowerReduction = 1;
-            }
-            
-            if ((gamepad1.left_trigger > 0.25)){
-                robot.ssScrew.setPower(-(gamepad1.left_trigger/1.5));
-            }if ((gamepad1.right_trigger > 0.25) && (robot.ssScrew.getCurrentPosition() <= 2230)){
-                robot.ssScrew.setPower(gamepad1.right_trigger);   
-            }else{
-                robot.ssScrew.setPower(0);
             }
             
             if(gamepad1.b){
@@ -93,75 +86,80 @@ public class TeleOp_Mec_Official_NewRobot extends LinearOpMode {
             telemetry.addData("rightY", robot.getRightYEncoder());
             telemetry.update();
             
-            //
-            //
-            //
-            //                                                           
+            
             //////////////////////////// DRIVER 2 ------------------ DRIVER 2 //////////////////////
-                                                                          
-          if (Math.abs(gamepad2.left_stick_y) > 0.05){
-              robot.Lelevator.setPower(gamepad2.left_stick_y);
-              robot.Relevator.setPower(-gamepad2.left_stick_y);
-          }
-          else if (gamepad2.right_bumper){
-              robot.Lintake.setPower(-0.6);
-              robot.Rintake.setPower(0.6);
-              
-              //robot.frontGate.setPosition();/// open 
-          }
-          else if (gamepad2.left_bumper){
-              robot.Lintake.setPower(0);
-              robot.Rintake.setPower(0);
-              
-             // robot.frontGate.setPosition();/// close 
-          }
-          
-          else if (gamepad2.y){
-              while (robot.Lelevator.getCurrentPosition() < 1000 && robot.Relevator.getCurrentPosition() < 1000 ){
-                    robot.Lelevator.setPower(0.25); //// up
-                    robot.Relevator.setPower(-0.25); //// up
+                
+            //This is newstuff
+            if (gamepad.left_stick_y > 0)
+            
+            //this is end of new stuff
+                                                                              
+            if (Math.abs(gamepad2.left_stick_y) > 0.05){
+                  robot.Lelevator.setPower(gamepad2.left_stick_y);
+                  robot.Relevator.setPower(-gamepad2.left_stick_y);
+            }
+            else if (Math.abs(gamepad2.right_stick_y) > 0.05){
+                  robot.headExtend.setPower(gamepad2.right_stick_y);
+              }
+            
+              if (gamepad2.right_bumper){
+                  robot.Lintake.setPower(-0.6);
+                  robot.Rintake.setPower(0.6);
+                  
+                  //robot.frontGate.setPosition();/// open 
+              }
+              if (gamepad2.left_bumper){
+                  robot.Lintake.setPower(0);
+                  robot.Rintake.setPower(0);
+                  
+                 // robot.frontGate.setPosition();/// close 
               }
               
-             // robot.frontGate.setPosition();/// Kick
-             // robot.rearRamp.setPosition();/// Dump
-          }
-          
-        //   else if (Math.abs(gamepad2.right_stick_y) > 0.05){
-        //       robot.headExtend.setPower(gamepad2.right_stick_y);
-        //   }
-        
-        else if (gamepad2.a){
-             // robot.frontGate.setPosition();/// Open
-        }
-         else if (gamepad2.x){
-             //robot.releaseCap.setPosition();///// Release
-        }
-         else if (gamepad2.a){
-             while (robot.Lelevator.getCurrentPosition() < 20 && robot.Relevator.getCurrentPosition() < 20 ){
-                    robot.Lelevator.setPower(-0.25);  /// down
-                    robot.Relevator.setPower(0.25);  //// down
-             }
-             // robot.frontGate.setPosition();/// Open
-              // robot.rearRamp.setPosition();/// level
-             
-            /// robot.headExtend = in     /// not sure how
-            
-        }
-        
-        else if (gamepad2.dpad_up){
-            robot.headExtend.setPower(0.55);
-        } else if (gamepad2.dpad_down){
-            robot.headExtend.setPower(0.45);
-        }
-        
-
-
-            //////////////////////////// DRIVER 1 &&&&&&&&&&&&&&&&& DRIVER 2 //////////////////////
-
-            if(gamepad1.x && gamepad2.x){
-                robot.ssClaw.setPosition(-1);    //NUKE
+              if (gamepad2.y){
+                  while (robot.Lelevator.getCurrentPosition() < 1000 && robot.Relevator.getCurrentPosition() < 1000 ){
+                        robot.Lelevator.setPower(0.25); //// up
+                        robot.Relevator.setPower(-0.25); //// up
+                  }
+                 // robot.frontGate.setPosition();/// Kick
+                 // robot.rearRamp.setPosition();/// Dump
+              }
+              
+             if (gamepad2.a){
+                 // robot.frontGate.setPosition();/// Open
+            }
+              if (gamepad2.x){
+                 //robot.releaseCap.setPosition();///// Release
             }
             
+            //  if (gamepad2.b){
+            //      while (robot.Lelevator.getCurrentPosition() < 20 && robot.Relevator.getCurrentPosition() < 20 ){
+            //             robot.Lelevator.setPower(-0.25);  /// down
+            //             robot.Relevator.setPower(0.25);  //// down
+            //      }
+            //      robot.Lelevator.setPower(0);  /// off
+            //      robot.Relevator.setPower(0); //// off
+            //      // robot.frontGate.setPosition();/// Open
+            //       // robot.rearRamp.setPosition();/// level
+                 
+            //     /// robot.headExtend = in     /// not sure how
+                
+            // }
+          
+            
+             if (gamepad2.dpad_up){
+                robot.headExtend.setPower(0.55);
+            } else if (gamepad2.dpad_down){
+                robot.headExtend.setPower(0.45);
+            }
+            
+    
+    
+                //////////////////////////// DRIVER 1 &&&&&&&&&&&&&&&&& DRIVER 2 //////////////////////
+    
+                if(gamepad1.x && gamepad2.x){
+                    robot.releaseCap.setPosition(-1);    //NUKE
+                }
+                
             
         
        }
